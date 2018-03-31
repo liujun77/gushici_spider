d50a19e66f09b9ea70bd2027feedaba1256c2f9a
junliu <email@example.com>
2018-03-30 18:14:50 -0700

    update spider

--- gushici_spider/spiders/sc_spider.py
+++ gushici_spider/spiders/sc_spider.py
@@ -1,13 +1,22 @@
 import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
 
-class SC_Spider_Xpath(scrapy.Spider):
+class GscSpider(CrawlSpider):
 
-    name = "sc_spider_xpath"
+    name = "sc_spider"
     start_urls = ["https://www.gushiwen.org/"]
     allowed_dpmians = ["gushiwen.org"]
+    rules = (
+        Rule(LinkExtractor(allow=('/authors/*',),
+                           deny=('/guwen/*', '/mingju/*')), follow=True),
+        Rule(LinkExtractor(allow=('/shiwenv_*')), callback='parse_item'),
+    )
 
-    def parse(self, response):
-
-        for next_url in response.xpath("//a/@href").extract():
-            if next_url is not None:
-                yield scrapy.Request(response.urljoin(next_url))
+    def parse_item(self, response):
+        """
+        parse poem
+        """
+        item = scrapy.Item()
+        print response.url + "liujun77"
+        return item
