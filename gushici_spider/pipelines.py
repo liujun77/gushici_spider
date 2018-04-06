ab21e377a8224d315a4b831dfcc8f1999cfa62f9
junliu <email@example.com>
2018-04-06 16:53:59 -0700

    gushiwen.org work with bugs

--- gushici_spider/pipelines.py
+++ gushici_spider/pipelines.py
@@ -5,15 +5,15 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
-
+import codecs
 
 class GushiciSpiderPipeline(object):
 
     def __init__(self):
-        self.output_file = open("output.json", 'wb')
+        self.output_file = codecs.open('out.json', 'w', encoding='utf-8')
 
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
         self.output_file.write(line)
         return item
 
