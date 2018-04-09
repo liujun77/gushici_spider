40e5b510398b859de0177a6400927566288d16d5
Jun Liu <liujun77@gmail.com>
2018-04-09 11:25:06 -0700

    update

--- gushici_spider/spiders/sc_spider.py
+++ gushici_spider/spiders/sc_spider.py
@@ -10,23 +10,39 @@ class GscSpider(CrawlSpider):
     start_urls = ["https://sou-yun.com/PoemIndex.aspx?"]
     allowed_dpmians = ["sou-yun.com"]
     rules = (
-        Rule(LinkExtractor(allow=('/authors/*',),
-                           deny=('/guwen/*', '/mingju/*')), follow=True),
-        Rule(LinkExtractor(allow=('/shiwenv_*')), callback='parse_item'),
+        Rule(LinkExtractor(allow=('/P*dynasty=*author=*type=*')),
+             callback='parse_item'),
+        Rule(LinkExtractor(allow=('/P*dynasty=*author=*')),
+             callback='parse_author'),
+        Rule(LinkExtractor(allow=('/P*dynasty=*')),
+             callback='parse_dynasty'),
     )
 
     def parse_dynasty(self, response):
-        pass
+
+        authors = response.xpath('//div[contains(@class, "list1")]')
+        authors_url = authors.xpath('div[contains(@class, "inline")]/a/@href').extract()
+        for author_url in authors_url:
+            yield scrapy.Request(response.urljoin(author_url))
+
+    def parse_author(self, response):
+        types = response.xpath('//a[contains(@class, "list")]/@href').extract()
+        for typ in types:
+            yield scrapy.Request(response.urljoin(typ))
 
     def parse_item(self, response):
         """
         parse poem
         """
-        item = GushiciSpiderItem()
-        item['url'] = re.search('.*shiwenv_(\w+).aspx', response.url).group(1)
-        item['title'] = response.xpath('//h1[contains(@style, "font-size:20px")]/text()').extract()[0]
-        item['author'] = response.xpath('//p/a[contains(@href, "authorv")]/text()').extract()[0]
-        item['era'] = response.xpath('//p/a[contains(@href, "shiwen")]/text()').extract()[0]
+        items = response.xpath('//div[contains(@id, "item")]')
+        for it in items:
+            item = GushiciSpiderItem()
+            item['title'] = []
+            for title in it.xpath('div[contains(@class, "title")]/text()').extract():
+                item['title'].append(title)
+            item['url'] = re.search('.*shiwenv_(\w+).aspx', response.url).group(1)
+            item['author'] = response.xpath('//p/a[contains(@href, "authorv")]/text()').extract()[0]
+            item['era'] = response.xpath('//p/a[contains(@href, "shiwen")]/text()').extract()[0]
 
         cont_resp = response.xpath('//div[contains(@id, "contson")]')[0]
         pre_text = cont_resp.xpath('p/span/text()').extract()
@@ -41,4 +57,14 @@ class GscSpider(CrawlSpider):
         item['text'] = []
         for sentance in text:
             item['text'].append(sentance)
-        return item
+        yield item
+
+        #generate next page
+        curr_str = re.search('.*=(\d+)', response.url)
+        curr = int(curr_str.group(1)) if curr_str is not None else 0
+        nex = response.xpath('//div[contains(@class, "poem")]/a/@href').extract()
+        if nex is None:
+            return
+        next_n = int(re.search('.*=(\d+)', nex[-1]).group(1))
+        if next_n == curr + 1:
+            yield scrapy.Request(response.urljoin(nex[-1]))
